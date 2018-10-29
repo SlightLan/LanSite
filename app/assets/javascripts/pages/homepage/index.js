195e09f1013de9cd4c5d486f357f64b7974f601c
Lan <324138594@qq.com>
2018-10-30 01:24:18 +0800

    try twinkle star

--- app/assets/javascripts/pages/homepage/index.js
+++ app/assets/javascripts/pages/homepage/index.js
@@ -1,8 +1,7 @@
 console.log('This is homepage with canvas');
 
 function drawStar(cxt, x, y, scale, color = '#FFFFFF') {
-    cxt.moveTo(x, 0.5*scale + y)
-    cxt.lineTo(0.45*scale + x, 0.45*scale + y);
+    cxt.moveTo(0.45*scale + x, 0.45*scale + y);
     cxt.lineTo(0.5*scale + x, y);
     cxt.lineTo(0.55*scale + x, 0.45*scale + y);
     cxt.lineTo(scale + x, 0.5*scale + y);
@@ -10,6 +9,7 @@ function drawStar(cxt, x, y, scale, color = '#FFFFFF') {
     cxt.lineTo(0.5*scale + x, scale + y);
     cxt.lineTo(0.45*scale + x, 0.55*scale + y);
     cxt.lineTo(x, 0.5*scale + y);
+    cxt.lineTo(0.45*scale + x, 0.45*scale + y);
     cxt.fillStyle = '#FFFFFF';
     cxt.fill();
     cxt.strokeStyle = color;
@@ -17,8 +17,8 @@ function drawStar(cxt, x, y, scale, color = '#FFFFFF') {
 }
 
 function drawNarrowStar(cxt, x, y, scale, color = '#FFFFFF') {
-    cxt.moveTo(0.2*scale + x, 0.5*scale + y)
-    cxt.lineTo(0.45*scale + x, 0.45*scale + y);
+
+    cxt.moveTo(0.45*scale + x, 0.45*scale + y);
     cxt.lineTo(0.5*scale + x, y);
     cxt.lineTo(0.55*scale + x, 0.45*scale + y);
     cxt.lineTo(0.8*scale + x, 0.5*scale + y);
@@ -26,21 +26,40 @@ function drawNarrowStar(cxt, x, y, scale, color = '#FFFFFF') {
     cxt.lineTo(0.5*scale + x, scale + y);
     cxt.lineTo(0.45*scale + x, 0.55*scale + y);
     cxt.lineTo(0.2*scale + x, 0.5*scale + y);
+    cxt.lineTo(0.45*scale + x, 0.45*scale + y);
     cxt.fillStyle = '#FFFFFF';
     cxt.fill();
     cxt.strokeStyle = color;
     cxt.stroke();
 }
 
+function starTwinkle(star, cxt) {
+    let starCanvaColor = document.getElementById('star_canvas_color');
+    let draw = function() {
+        cxt.clearRect(0, 0, star.width, star.height);
+        drawStar(cxt, 5, 5, 10, window.getComputedStyle(starCanvaColor).backgroundColor);
+        requestAnimationFrame(draw);
+        console.log('color is:', window.getComputedStyle(starCanvaColor).backgroundColor);
+    };
+    draw();
+}
+
 function initCanva() {
-    var canvas = document.getElementById('background_star_sky');
-    var cxt = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    cxt.fillStyle = '#66CCFF';
-    cxt.fillRect(0, 0, canvas.width, canvas.height);
-    drawStar(cxt, 0, 0, 80, '#FFFF00');
-    drawNarrowStar(cxt, 540, 210, 40, '#FFFF00');
+    let canvas = document.getElementsByClassName('background_star_sky');
+    let bg = document.createElement('div');
+    canvas[0].appendChild(bg);
+    bg.style.width = window.innerWidth.toString()+'px';
+    bg.style.height = window.innerHeight.toString()+'px';
+    for(let starNum = 0; starNum < 30; starNum += 1) {
+        let star = document.createElement('canvas');
+        star.className = 'star_canvas';
+        star.id = starNum.toString();
+        star.width = 20;
+        star.height = 20;
+        let starCxt = star.getContext('2d');
+        starTwinkle(star, starCxt);
+        bg.appendChild(star);
+    }
 }
 
 document.addEventListener('DOMContentLoaded', initCanva);

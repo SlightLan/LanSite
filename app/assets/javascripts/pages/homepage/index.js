40cd448dc08112a2b4fbfbe1c31ebb5edbd2ace6
Lan <lan@qq.com>
2018-11-20 23:14:37 +0800

    Add resize event

--- app/assets/javascripts/pages/homepage/index.js
+++ app/assets/javascripts/pages/homepage/index.js
@@ -1,6 +1,13 @@
-import $ from 'expose?$!jquery'
 import initCanva from './star_twinkle'
 
 document.addEventListener('DOMContentLoaded', initCanva);
 
-const btn = $('enter_btn');
+window.onresize = function(){
+    let canvas = document.getElementsByClassName('background_star_sky');
+    let bg = canvas[0].firstChild;
+    for(let i = bg.childNodes.length; i > 0; i--){
+        bg.removeChild(bg.firstChild);
+    }
+    canvas[0].removeChild(bg);
+    initCanva();
+};

a96d02ace4bfe7dba08ce5ff9a5902b8b1654730
Lan <324138594@qq.com>
2018-11-27 13:18:31 +0800

    refix shortmessage

--- app/controllers/http_request_controller.rb
+++ app/controllers/http_request_controller.rb
@@ -1,7 +1,7 @@
 class HttpRequestController < ApplicationController
   def show
-    @accesskey = "AccseeKey"
-    @secretkey = "SecretKey" + "&"
+    @accesskey = "LTAIpkZFyfUpaDlQ"
+    @secretkey = "aYUNeSzJRfbuNGEFiK0TmylM7KDaam" + "&"
     Time.zone = "UTC"
     @time = Time.zone.now.strftime('%Y-%m-%dT%H:%M:%SZ')
     @hash = Hash.new
@@ -15,31 +15,35 @@ class HttpRequestController < ApplicationController
     @hash["Action"] = "SendSms"
     @hash["Version"] = "2017-05-25"
     @hash["RegionId"] = "cn-hangzhou"
-    @hash["PhoneNumbers"] = "13217314907"
+    @hash["PhoneNumbers"] = "15300000001"
     @hash["TemplateCode"] = "SMS_147201673"
-
+    # @hash["TemplateParam"] = "{\"customer\":\"test\"}".encode("utf-8")
+    # @hash["SignName"] = "阿里云短信测试专用".encode("utf-8")
+    # @hash["OutId"] = "123"
     @hash = @hash.sort_by{|key, val|key}
     @final_params = ""
-    @params = "GET&/"
+    @params = ""
     @hash.each do |hash1|
       @params += "&" + hash1[0] + "=" + hash1[1]
       @final_params += "&" + hash1[0] + "=" + hash1[1]
     end
-
-    Rails::logger.info(@params)
+    @params = @params[1, @params.length]
     @params = URI.escape(@params, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
+    @pre_params = "GET&amp;%2F&amp;"
+    @params = @pre_params + @params
     Rails::logger.info(@params)
 
     key = @secretkey.encode("utf-8")
-    digest = OpenSSL::Digest::Digest.new('sha1')
+    digest = OpenSSL::Digest.new('sha1')
     string_to_sign = @params.encode("utf-8")
     @hashed = Base64.encode64(OpenSSL::HMAC.digest(digest, key, string_to_sign))
 
-    @final_params = @hashed.chop + @final_params
     @final_params = URI.escape(@final_params, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-    @final_params = @final_params.gsub("%3D","=")
-    @final_params = @final_params.gsub("%26","&")
+    @final_params = @final_params.gsub("%3D", "=")
+    @final_params = @final_params.gsub("%26", "&")
+    @final_params =URI.escape(@hashed.chop, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")) + @final_params
     Rails::logger.info(@final_params)
+
 
     @url = "http://dysmsapi.aliyuncs.com/?Signature=" + @final_params
     request = Typhoeus::Request.new(
